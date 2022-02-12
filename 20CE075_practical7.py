faf431919213cd1a340b7b220e23ce00625a3d50
Morningstar000 <96479756+Morningstar000@users.noreply.github.com>
2022-02-12 22:05:15 +0530

    Update 20CE075_practical7.py

--- 20CE075_practical7.py
+++ 20CE075_practical7.py
@@ -5,7 +5,7 @@
 # since the two halves ga and ga have the same characters with same frequency. Also, abccab, rotor and xyzxy are a few examples of lapindromes.
 # Note that abbaab is NOT a lapindrome. The two halves contain the same characters but their frequencies do not match.
 # Your task is simple. Given a string, you need to tell if it is a lapindrome.
-# 
+# https://github.com/Morningstar000/Python-Practical-7
 
 t = int(input())
 dict0 = {}
